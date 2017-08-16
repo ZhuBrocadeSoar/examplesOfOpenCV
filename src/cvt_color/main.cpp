be2ebb33ea13d6f3628fafcb504571e55e25b6c2
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-08-16 16:47:43 +0800

    pause editing cvt_color,because i find something not good in codes,i will write mans of funcs down in the README

---
+++ src/cvt_color/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: ../src/cvt_color/main.cpp
+	> Author: Zhu Brocadesoar
+	> Mail: brocadesoar@gmail.com
+	> Created Time: Wed 16 Aug 2017 04:35:40 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/>
+
